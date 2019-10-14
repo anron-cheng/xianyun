55ca516345780cff7d0adcc44f18e8e9054c8bf3
anron-cheng <53592617+anron-cheng@users.noreply.github.com>
2019-10-14 17:27:32 +0800

    设置axios错误拦截器

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,3 +1,13 @@
-export default ()=>{
+// axios错误拦截器
+import { Message } from 'element-ui';
+export default (context)=>{
+    context.$axios.onError(res=>{
+        
+        const {message,statusCode} = res.response.data
+        if(statusCode==400){
+            Message.error(message)
+        }
+    });
+    
     
 }
